6d29977f73de74b4b075f105d4ead853fc1d73ba
Rehan Theiveehathasan <theivers@mcmaster.ca>
2018-08-19 23:45:44 -0400

    HSR_GTFS.cs now receives HTML data of URLs
    Rewrote code to fit Unity 5.6 archived documentation

--- Assets/Scripts/HSR_GTFS.cs
+++ Assets/Scripts/HSR_GTFS.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class HSR_GTFS : MonoBehaviour 
 {
+	// URLs
+	private const string GTFSHamiltonURL = "http://opendata.hamilton.ca/GTFS-RT/";
+	private const string ServiceAlertsURL = "http://opendata.hamilton.ca/GTFS-RT/GTFS_ServiceAlerts.pb";				// URL for GTFS_ServiceAlerts.pb
+	private const string TripUpdatesURL = "http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb";					// URL for GTFS_TripUpdates.pb
+	private const string VehiclePositionsURL = "http://opendata.hamilton.ca/GTFS-RT/GTFS_VehiclePositions.pb";			// URL for GTFS_VehiclePositions.pb
 
 	// Use this for initialization
 	void Start () 
@@ -21,51 +26,16 @@ public class HSR_GTFS : MonoBehaviour
 	IEnumerator GetData()
 	{
 		// Get data from GTFS directory on opendata.hamilton.ca for transit information
-		using (UnityWebRequest www1 = UnityWebRequest.Get ("http://opendata.hamilton.ca/GTFS-RT/GTFS_ServiceAlerts.pb"))
-		{
+		UnityWebRequest www1 = new UnityWebRequest (GTFSHamiltonURL);
+		www1.downloadHandler = new DownloadHandlerBuffer ();
+		yield return www1.Send ();
 
-			// Currently debugging why bytes aren't being downloaded, as of now string test is 0 bytes long!
-
-			yield return www1.Send ();
-
-			byte[] ServiceAlerts = www1.downloadHandler.data;
-
-			string test = www1.downloadHandler.text;
-			Debug.Log (test.Length);
-			/*
-			if (www1.isError) {
-				Debug.Log (www1.error);
-			} else {
-				byte[] ServiceAlerts = www1.downloadHandler.data;
-				if (www1.isDone) {Debug.Log ("ServiceAlerts has finished downloading."); Debug.Log (www1.downloadHandler.text);}
-			}
-			*/			
+		if (www1.isError) {
+			Debug.Log (www1.error);
+		} else {
+			Debug.Log (www1.downloadHandler.text);
+			byte[] GTFSHamilton = www1.downloadHandler.data;
 		}
-				
-		using (UnityWebRequest www2 = UnityWebRequest.Get("http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb"))
-		{
-			yield return www2.Send ();
-
-			if (www2.isError) {
-				Debug.Log (www2.error);
-			} else {
-				byte[] TripUpdates = www2.downloadHandler.data;
-				if (www2.isDone) {Debug.Log ("TripUpdates has finished downloading."); Debug.Log (www2.downloadHandler.text);}
-			}			
-		}
-
-		using (UnityWebRequest www3 = UnityWebRequest.Get("http://opendata.hamilton.ca/GTFS-RT/GTFS_VehiclePositions.pb"))
-		{
-			yield return www3.Send ();
-
-			if (www3.isError) {
-				Debug.Log (www3.error);
-			} else {
-				byte[] VehiclePositions = www3.downloadHandler.data;
-				if (www3.isDone) {Debug.Log ("VehiclePositions has finished downloading."); Debug.Log (www3.downloadHandler.text);}
-			}			
-		}
-
 
 	}
 
